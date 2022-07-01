ee6788193c545e75444ce8c974f93edb3fc3e2e1
Lucas de Sena <36707595+lucasbnu@users.noreply.github.com>
2022-07-01 18:37:36 -0300

    Commit projetos primeira aula

---
+++ Java fundamentos/DeclarandoMetodos/src/main/java/apex/DeclarandoMetodos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package apex;
+
+/**
+ *
+ * @author lucas
+ */
+public class DeclarandoMetodos {
+    
+public static int somaNumeroEImprime(int numero1, int numero2){
+    int soma = numero1 + numero2;
+    System.out.println(soma);
+    return soma;
+}
+    public static void main(String[] args) {
+       int Retorno = somaNumeroEImprime(4,5);
+        System.out.println(Retorno + 10);
+    }
+    
+}
